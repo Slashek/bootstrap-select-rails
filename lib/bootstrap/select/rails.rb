6d7c890561b0a3d88a2533750844098af40e60ab
Maciej Krajowski-Kukiel <maciej.krajowski@gmail.com>
2013-05-24 19:29:50 +0200

    initial commit

---
+++ lib/bootstrap/select/rails.rb
@@ -0,0 +1,9 @@
+require "bootstrap/select/rails/version"
+
+module Bootstrap
+  module Select
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bootstrap/select/rails/version.rb
@@ -0,0 +1,7 @@
+module Bootstrap
+  module Select
+    module Rails
+      VERSION = "1.3.0"
+    end
+  end
+end
